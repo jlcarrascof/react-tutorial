9597827fd6652f7055211b6133bdf089ff305333
Javier Martinez <entrenamientospuntonet@gmail.com>
2023-08-15 18:45:32 -0400

    Fix Linter errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,40 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
-    const headerStyle = {
-        padding: "20px 0",
-        lineHeight: "1.5em",
-      }
-    return (
-        <header style={headerStyle}>
-            <h1 
-             style={{
-                fontSize: "6rem",
-                fontWeight: "600",
-                marginBottom: "2rem",
-                lineHeight: "1em",
-                color: "#ececec",
-                textTransform: "lowercase",
-                textAlign: "center",
-              }}
-            >Todos</h1>
-            <h3 
-             style={{
-                fontSize: "1.5rem",
-                fontWeight: "600",
-                marginBottom: "2rem",
-                lineHeight: "1em",
-                color: "#ececec",
-                textTransform: "lowercase",
-                textAlign: "center",
-              }}
-            >Items will persist in the browser local storage</h3>
-        </header>
-    )
-}
+  const headerStyle = {
+    padding: '20px 0',
+    lineHeight: '1.5em',
+  };
+  return (
+    <header style={headerStyle}>
+      <h1
+        style={{
+          fontSize: '6rem',
+          fontWeight: '600',
+          marginBottom: '2rem',
+          lineHeight: '1em',
+          color: '#ececec',
+          textTransform: 'lowercase',
+          textAlign: 'center',
+        }}
+      >
+        Todos
+      </h1>
+      <h3
+        style={{
+          fontSize: '1.5rem',
+          fontWeight: '600',
+          marginBottom: '2rem',
+          lineHeight: '1em',
+          color: '#ececec',
+          textTransform: 'lowercase',
+          textAlign: 'center',
+        }}
+      >
+        Items will persist in the browser local storage
+      </h3>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import TodoContainer from "./components/TodoContainer"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './components/TodoContainer';
 
-//stylesheet
-import "./App.css"
+// stylesheet
+import './App.css';
 
-
-ReactDOM.render(<React.StrictMode><TodoContainer /></React.StrictMode>, document.getElementById("root"))
-
+ReactDOM.render(<React.StrictMode><TodoContainer /></React.StrictMode>, document.getElementById('root'));
